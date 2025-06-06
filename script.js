f27a52b787592a2051c187079c450af423d5183a
Rafaellcosta <127904521+Rafaellcosta@users.noreply.github.com>
2025-06-05 22:00:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function mostrarPopup() {
 function fecharPopup() {
     document.getElementById('popup').style.display = 'none';
 }
+function criarCorações() {
+    for (let i = 0; i < 15; i++) {
+        let coracao = document.createElement("div");
+        coracao.classList.add("heart");
+        coracao.style.left = `${Math.random() * 100}vw`;
+        coracao.style.animationDuration = `${Math.random() * 5 + 3}s`;
+        document.body.appendChild(coracao);
+    }
+}
+
+criarCorações();
